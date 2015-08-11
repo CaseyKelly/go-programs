a04b33802f519cf58adfc30f68d23aa9900e07fe
Casey Kelly <casey.kelly@colorado.edu>
2015-08-11 16:52:47 -0400

    add random number generator

--- hello.go
+++ hello.go
@@ -2,12 +2,22 @@
 package main
 
 import (
-	"fmt"  // a package in the Go standard library, means format
-	"time" // Package time provides functionality for measuring and displaying time.
+	// a package in the Go standard library, means format
+	"fmt"
+
+	// Package math provides basic constants and mathematical functions.
+	"math/rand"
+
+	// Package time provides functionality for measuring and displaying time. Package rand implements pseudo-random number generators.
+	"time"
 )
 
 func main() {
 	fmt.Println("Hello World!")
+
 	t := time.Now()
 	fmt.Println(t.Format("Today is Monday, January 2, 2006. The time is 3:04 PM."))
+
+	rand.Seed(t.Unix())
+	fmt.Println(rand.Intn(10), "is a random number")
 }
