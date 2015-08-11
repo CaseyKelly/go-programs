ae64cd9c52864a36a06ab84e267a6e32a3a7e2f8
Casey Kelly <casey.kelly@colorado.edu>
2015-08-11 16:22:22 -0400

    add formatted date and time

--- hello.go
+++ hello.go
@@ -1,8 +1,13 @@
 // package clause starts every source file
 package main
 
-import "fmt"       // a package in the Go standard library
+import (
+  "fmt"   // a package in the Go standard library, means format
+  "time"  // Package time provides functionality for measuring and displaying time.
+  )
 
 func main() {
   fmt.Println("Hello World!")
+  t := time.Now()
+  fmt.Println(t.Format("Today is Monday, January 2, 2006. The time is 3:04 PM."))
 }
