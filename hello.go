452b4c12a2b9147eb385ac41506bd83a5a584145
Casey Kelly <casey.kelly@colorado.edu>
2015-08-11 16:24:10 -0400

    go fmt'd

--- hello.go
+++ hello.go
@@ -2,12 +2,12 @@
 package main
 
 import (
-  "fmt"   // a package in the Go standard library, means format
-  "time"  // Package time provides functionality for measuring and displaying time.
-  )
+	"fmt"  // a package in the Go standard library, means format
+	"time" // Package time provides functionality for measuring and displaying time.
+)
 
 func main() {
-  fmt.Println("Hello World!")
-  t := time.Now()
-  fmt.Println(t.Format("Today is Monday, January 2, 2006. The time is 3:04 PM."))
+	fmt.Println("Hello World!")
+	t := time.Now()
+	fmt.Println(t.Format("Today is Monday, January 2, 2006. The time is 3:04 PM."))
 }
